60da01ec4a956d076914ed68023319214083cd46
Roy Shilkrot <roy.shil@gmail.com>
2024-11-26 21:53:16 -0500

    Update CMake configuration for Clova API: set output directory and adjust protoc command for generated files

--- cmake/BuildClovaAPIs.cmake
+++ cmake/BuildClovaAPIs.cmake
@@ -1,12 +1,14 @@
 set(PROTO_FILE ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova/nest.proto)
+set(CLOVA_OUTPUT_DIR ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova)
 
 message(STATUS "Generating C++ code from ${PROTO_FILE}")
 
 # run protoc to generate the grpc files
 add_custom_command(
   OUTPUT ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova/nest.pb.cc
-  COMMAND
-    ${PROTOC_EXECUTABLE} --cpp_out=${CMAKE_SOURCE_DIR}/src/cloud-providers/clova
-    --grpc_out=${CMAKE_SOURCE_DIR}/src/cloud-providers/clova --plugin=protoc-gen-grpc=${GRPC_PLUGIN_EXECUTABLE} -I
-    ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova ${PROTO_FILE}
+         ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova/nest.pb.h
+         ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova/nest.grpc.pb.cc
+         ${CMAKE_SOURCE_DIR}/src/cloud-providers/clova/nest.grpc.pb.h
+  COMMAND ${PROTOC_EXECUTABLE} --cpp_out=${CLOVA_OUTPUT_DIR} --grpc_out=${CLOVA_OUTPUT_DIR}
+          --plugin=protoc-gen-grpc=${GRPC_PLUGIN_EXECUTABLE} -I ${CLOVA_OUTPUT_DIR} ${PROTO_FILE}
   DEPENDS ${PROTO_FILE})
